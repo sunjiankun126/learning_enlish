1e9eef99a0645c30fc1f141d5d34454b91324f71
sunjiankun <sunjiankun@mail.pmlabs.com.cn>
2025-04-08 22:21:30 +0800

    [feature]增加建表语句

--- src/main/resources/mybatis/sql/schema.sql
+++ src/main/resources/mybatis/sql/schema.sql
@@ -3,7 +3,7 @@ USE `learning_english`;
 
 CREATE TABLE  IF NOT EXISTS users
 (
-    user_id      bigint PRIMARY KEY AUTO_INCREMENT ,                                 -- 唯一标识符
+    id      int PRIMARY KEY AUTO_INCREMENT ,                                 -- 唯一标识符
     nick_name    VARCHAR(255) NOT NULL,                                          -- 用戶昵称
     openid       VARCHAR(255) NOT NULL,                                        -- 诗歌作者
     phone_num     VARCHAR(50),                                                    -- 诗歌体裁
@@ -15,7 +15,7 @@ CREATE TABLE  IF NOT EXISTS users
 
 CREATE TABLE  IF NOT EXISTS words
 (
-    word_id  int PRIMARY KEY AUTO_INCREMENT ,                                 -- 唯一标识符
+    id  MEDIUMINT PRIMARY KEY AUTO_INCREMENT ,                                 -- 唯一标识符
     name VARCHAR(255) NOT NULL,
     trans VARCHAR(2048) NOT NULL,
     uk_phone VARCHAR(255) default NULL,
@@ -25,27 +25,38 @@ CREATE TABLE  IF NOT EXISTS words
     updated_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- 更新时间
     )ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '用戶表' ROW_FORMAT = Dynamic;
 
-CREATE TABLE IF NOT EXISTS user_words (
-    user_id bigint,
-    word_id INT,
-    status ENUM('unlearned', 'learning', 'learned') DEFAULT 'unlearned',  -- 学习状态
-    review_count INT DEFAULT 0,          -- 复习次数
-    last_reviewed DATETIME,              -- 上次复习时间
-    PRIMARY KEY (user_id, word_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (word_id) REFERENCES words(word_id)
-)ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '学习进度表' ROW_FORMAT = Dynamic;
 
+CREATE TABLE IF NOT EXISTS wrong_words (
+          user_id int,
+          word_id MEDIUMINT,
+          review_count INT DEFAULT 0,          -- 复习次数
+          last_reviewed DATETIME,              -- 上次复习时间
+          PRIMARY KEY (user_id, word_id),
+          FOREIGN KEY (user_id) REFERENCES users(id),
+          FOREIGN KEY (word_id) REFERENCES words(id)
+)ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '错题本' ROW_FORMAT = Dynamic;
 
-CREATE TABLE IF NOT EXISTS  dict (
-       id VARCHAR(50) NOT NULL PRIMARY KEY COMMENT '唯一标识符',
+CREATE TABLE IF NOT EXISTS favorite_words (
+           user_id int,
+           word_id MEDIUMINT,
+           review_count INT DEFAULT 0,          -- 复习次数
+           last_reviewed DATETIME,              -- 上次复习时间
+           PRIMARY KEY (user_id, word_id),
+           FOREIGN KEY (user_id) REFERENCES users(id),
+           FOREIGN KEY (word_id) REFERENCES words(id)
+)ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '收藏表' ROW_FORMAT = Dynamic;
+
+CREATE TABLE IF NOT EXISTS  dictionary (
+       id SMALLINT NOT NULL PRIMARY KEY COMMENT '唯一标识符',
        name VARCHAR(100) NOT NULL COMMENT '考试名称',
-       description TEXT COMMENT '考试描述',
-       category VARCHAR(100) COMMENT '类别',
-       tags JSON COMMENT '标签',
-       url VARCHAR(255) COMMENT '文件链接',
-       length INT COMMENT '单词总数',
-       translate_language VARCHAR(50) COMMENT '翻译语言',
-       language VARCHAR(50) COMMENT '语言',
-       type VARCHAR(50) COMMENT '字典类型'
+       created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '字典类型' ROW_FORMAT = Dynamic;
+
+CREATE TABLE IF NOT EXISTS  dict_word_relation (
+         word_id MEDIUMINT NOT NULL COMMENT '单词id',
+         dictionary_id SMALLINT NOT NULL COMMENT '词典Id',
+         PRIMARY KEY (word_id, dictionary_id),
+         FOREIGN KEY (dictionary_id) REFERENCES dictionary(id),
+         FOREIGN KEY (word_id) REFERENCES words(id)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '字典单词表' ROW_FORMAT = Dynamic;
+
